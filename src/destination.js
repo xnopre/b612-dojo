6be85ea691ec50955042d2543638114e0e7934a0
Cédric Guérin <cguerin@twinppek.net>
2019-02-07 13:46:10 +0100

    second solution
    Signed-off-by: Cédric Guérin <cguerin@twinppek.net>

--- src/destination.js
+++ src/destination.js
@@ -1,23 +1,10 @@
 export default function (points, nbDestination, maxTruckSize) {
-    function distance(point) {
-        return Math.sqrt(point[0] * point[0] + point[1] * point[1]);
-    }
-
     if (nbDestination > maxTruckSize) {
         throw Error("Camion trop petit")
     }
 
-    const pointsAndDistances = points.map((point) => ({
-        distance: distance(point),
-        point
-    }));
+    const sortedDistance = points.sort(([xa, ya], [xb, yb]) => (xa + ya) - (xb + yb));
 
-    const pointsAndDistancesSorted = pointsAndDistances.sort((a, b) => {
-        return a.distance > b.distance ? 1 : -1;
-    });
-
-    const pointsSorted = pointsAndDistancesSorted.map(a => a.point);
-
-    return pointsSorted.slice(0, nbDestination);
+    return sortedDistance.slice(0, nbDestination);
 
 }
